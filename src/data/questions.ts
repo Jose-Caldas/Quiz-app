a94d5fb7a602e0b3a0a45b6aae36cfe0c213ba88
José Caldas <jrcaldas@yahoo.com.br>
2023-07-13 15:49:52 -0300

    update data file

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -1,34 +1,63 @@
 const data = [
   {
-    question: 'O que é Vanilla JavaScript?',
-    options: [
-      'JavaScript puro',
-      'Uma biblioteca JavaScript',
-      'Um framework JavaScript',
-      'Um compilador de JavaScript',
-    ],
-    answer: 'JavaScript puro',
-  },
-  {
-    question: 'Com qual instrução declaramos uma constante em JavaScript?',
-    options: ['const', 'let', 'var', 'define'],
-    answer: 'const',
-  },
-  {
-    question: 'Qual dos tipos de dados a seguir não existe em JavaScript?',
-    options: ['string', 'number', 'boolean', 'float'],
-    answer: 'float',
-  },
-  {
-    question: 'Qual dos métodos a seguir seleciona um elemento?',
-    options: ['querySelector', 'parseInt', 'sort', 'reduce'],
-    answer: 'querySelector',
+    question:
+      'Qual das seguintes opções é uma linguagem de programação amplamente utilizada para o desenvolvimento de software?',
+    options: ['HTML', 'CSS', 'JavaScript', 'XML'],
+    answer: 'JavaScript',
   },
   {
     question:
-      'Qual destas propriedades da a quantidade de elementos de um array?',
-    options: ['qty', 'length', 'items', 'index'],
-    answer: 'length',
+      'Qual das seguintes opções é uma plataforma de desenvolvimento de software de código aberto?',
+    options: ['Microsoft Windows', 'iOS', 'Linux', 'macOS'],
+    answer: 'Linux',
+  },
+  {
+    question:
+      'Qual das seguintes opções é um banco de dados relacional muito utilizado no desenvolvimento de software?',
+    options: ['MongoDB', 'PostgreSQL', 'Redis', 'Elasticsearch'],
+    answer: 'PostgreSQL',
+  },
+  {
+    question:
+      'Qual das seguintes opções é um protocolo de comunicação utilizado para transferir arquivos pela internet?',
+    options: ['FTP', 'SMTP', 'HTTP', 'DNS'],
+    answer: 'FTP',
+  },
+  {
+    question:
+      'Qual das seguintes opções é uma metodologia ágil de desenvolvimento de software?',
+    options: ['Scrum', 'Waterfall', 'Lean', 'Six Sigma'],
+    answer: 'Scrum',
+  },
+  {
+    question:
+      'Qual das seguintes opções é um sistema de controle de versão amplamente utilizado no desenvolvimento de software?',
+    options: ['Git', 'Subversion', 'Mercurial', 'CVS'],
+    answer: 'Git',
+  },
+  {
+    question:
+      'Qual das seguintes opções é uma ferramenta de automação de testes de software?',
+    options: ['Selenium', 'JIRA', 'Jenkins', 'Docker'],
+    answer: 'Selenium',
+  },
+  {
+    question:
+      'Qual das seguintes opções é uma linguagem de consulta amplamente utilizada em bancos de dados relacionais?',
+    options: ['SQL', 'JSON', 'Python', 'Ruby'],
+    answer: 'SQL',
+  },
+  {
+    question:
+      'Qual das seguintes opções é uma plataforma de virtualização amplamente utilizada no desenvolvimento de software?',
+    options: ['VirtualBox', 'Docker', 'VMware', 'Hyper-V'],
+    answer: 'Docker',
+  },
+  {
+    question:
+      'Qual das seguintes opções é um padrão amplamente utilizado para representar e trocar dados estruturados na web?',
+    options: ['XML', 'JSON', 'YAML', 'HTML'],
+    answer: 'JSON',
   },
 ]
 
